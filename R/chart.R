fb31eae6a30985d58101542a6c0aef22b4846b16
mhidalgo <mhidalgo@cipf.es>
2017-12-20 17:05:43 +0100

    Adding group.by functionality to color.nodes.per.differential.expression

--- R/chart.R
+++ R/chart.R
@@ -557,6 +557,11 @@ add.edge.colors <- function(pathigraph, pcomp, effector, up.col = "#ca0020",
 #' Samples must be ordered as in \code{results}
 #' @param group1.label String, label of the first group to be compared
 #' @param group2.label String, label of the second group to be compared
+#' @param group.by How to group the subpathways to be visualized. By default
+#' they are grouped by the pathway to which they belong. Available groupings
+#' include "uniprot", to group subpathways by their annotated Uniprot functions,
+#' "GO", to group subpathways by their annotated GO terms, and "genes", to group
+#' subpathways by the genes they include.
 #' @param colors Either a character vector with 3 colors (indicating,
 #' in this order, down-regulation, non-significance and up-regulation colors)
 #'  or a key name indicating the color scheme to be used. Options are:
@@ -583,6 +588,7 @@ add.edge.colors <- function(pathigraph, pcomp, effector, up.col = "#ca0020",
 #'
 node.color.per.differential.expression <- function(results, metaginfo, groups,
                                                    group1.label, group2.label,
+                                                   group.by = NULL,
                                                    colors = "classic",
                                                    conf = 0.05){
 
@@ -597,36 +603,38 @@ node.color.per.differential.expression <- function(results, metaginfo, groups,
     no.col <- colors[2]
     up.col <- colors[3]
 
-    cols <- lapply(names(results$by.path), function(path){
-        difexp.nod <- compute.difexp(results$by.path[[path]]$nodes.vals,
-                                     group1.label,
-                                     group2.label,
-                                     groups)
-        updown <- sapply(difexp.nod$statistic, function(x){
-            if(x < 0){
-                "down"
-            }else if(x > 0){
-                "up"
-            }else{
-                "both"
-            }
-        })
-        col <- get.colors.from.pval(updown,
-                                    difexp.nod$p.value,
-                                    up.col = up.col,
-                                    down.col = down.col,
-                                    no.col = no.col,
-                                    conf = conf)
-        names(col) <- rownames(results$by.path[[path]]$nodes.vals)
+    if(!is.null(group.by))
+        metaginfo <- get.pseudo.metaginfo(metaginfo, group.by = group.by)
+
+    difexp <- compute.difexp(results$all$nodes.vals, group1.label,
+                             group2.label, groups)
+    updown <- sapply(difexp$statistic, function(x){
+        if(x < 0){
+            "down"
+        }else if(x > 0){
+            "up"
+        }else{
+            "both"
+        }
+    })
+    node.colors <- get.colors.from.pval(updown,
+                                        difexp$p.value,
+                                        up.col = up.col,
+                                        down.col = down.col,
+                                        no.col = no.col,
+                                        conf = conf)
+    names(node.colors) <- rownames(results$all$nodes.vals)
+    cols <- lapply(metaginfo$pathigraphs, function(pg){
+        gen.nodes <- V(pg$graph)$name[V(pg$graph)$name %in% rownames(difexp)]
+        path.colors <- node.colors[gen.nodes]
         # Add function colors
-        pathgraph <- metaginfo$pathigraphs[[path]]$graph
-        toadd <- V(pathgraph)$name[!V(pathgraph)$name %in% rownames(difexp.nod)]
+        toadd <- V(pg$graph)$name[!V(pg$graph)$name %in% rownames(difexp)]
         coltoadd <- rep("white", length(toadd))
         names(coltoadd) <- toadd
-        col <- c(col, coltoadd)
-        return(col)
+        path.colors <- c(path.colors, coltoadd)
+        return(path.colors)
     })
-    names(cols) <- names(results$by.path)
+    names(cols) <- names(metaginfo$pathigraphs)
     return(cols)
 }
 
